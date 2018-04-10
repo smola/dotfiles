d795409ce88eca43f5ae98477425e17dda99c573
Santiago M. Mola <santi@mola.io>
2018-04-10 16:35:41 +0200

    fix pyenv.zsh

--- .oh-my-zsh-custom/pyenv.zsh
+++ .oh-my-zsh-custom/pyenv.zsh
@@ -3,7 +3,7 @@ if [[ -d $HOME/.pyenv ]]; then
 	export PATH="$HOME/.pyenv/bin:$PATH"
 fi
 
-if hash pyenv &> /dev/null ; then
+if which pyenv &> /dev/null; then
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 fi
