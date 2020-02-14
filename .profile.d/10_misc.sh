8d95f83078b7b59b8818c740ac0445fdbb376d5e
Santiago M. Mola <santi@mola.io>
2020-02-14 10:46:38 +0100

    move to Xsession, set alacritty

--- .profile.d/10_misc.sh
+++ .profile.d/10_misc.sh
@@ -1,7 +1,11 @@
 
+if which alacritty &> /dev/null; then
+	export TERMINAL=alacritty
+fi
 
 export LANG=en_US.UTF-8
 export EDITOR=vim
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
