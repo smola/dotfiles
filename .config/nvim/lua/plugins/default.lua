ffedfdeb1ef74cb0455d41dbb9424932c903eab3
Santiago M. Mola <santi@mola.io>
2023-09-06 18:06:02 +0200

    update .shrc

--- .config/nvim/lua/plugins/default.lua
+++ .config/nvim/lua/plugins/default.lua
@@ -15,4 +15,24 @@ return {
 	"neovim/nvim-lspconfig",
 	"mhartington/formatter.nvim",
 	"mfussenegger/nvim-lint",
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = true,
+					hide_gitignored = true,
+					hide_by_name = {
+						-- '.git',
+						-- '.DS_Store',
+						-- 'thumbs.db',
+					},
+					never_show = {},
+				},
+			},
+		},
+	},
 }
