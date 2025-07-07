541cd1f86ee0608479b3d729aab728ccad825186
Santiago Mola <santi@mola.io>
2025-07-07 17:50:22 +0200

    nvm updates

--- .shrc.d/80_nvm.sh
+++
@@ -1,8 +0,0 @@
-
-if [[ ! -d $HOME/.nvm ]]; then
-    return 0
-fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ]  && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
